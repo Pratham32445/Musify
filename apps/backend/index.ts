eb9e8a94988b1787b53807ed37be81fb614bddeb
Pratham mehta <mehtapratham49@gmail.com>
2025-03-30 12:25:32 +0530

    added the register flow of user

--- apps/backend/index.ts
+++ apps/backend/index.ts
@@ -4,6 +4,8 @@ import { UserRouter } from "./router/User.router";
 
 const app = express();
 
+app.use(express.json());
+
 app.use("/room",RoomRouter);
 app.use("/user",UserRouter);
 
--- apps/backend/router/User.router.ts
+++ apps/backend/router/User.router.ts
@@ -1,9 +1,48 @@
 import express from "express"
+import { signUpSchema } from "comman/types"
+import prismaClient from "db/client"
+import bcryprt from "bcryptjs"
 
 const router = express.Router();
 
-router.post("/get-user",(req,res) => {
+router.post("/sign-up", async (req, res) => {
+    const parsedBody = signUpSchema.safeParse(req.body);
+    if (!parsedBody.success) {
+        res.status(411).json({
+            message: "Invalid Inputs"
+        })
+        return;
+    }
+    const user = await prismaClient.user.findFirst({
+        where: {
+            email: parsedBody.data.email
+        }
+    })
+    if (user) {
+        res.status(401).json({
+            message: "User Already Exist"
+        })
+        return;
+    }
+    const hashedPassword = await bcryprt.hash(parsedBody.data.password, 10);
+    await prismaClient.user.create({
+        data: {
+            email: parsedBody.data.email,
+            password: hashedPassword,
+            firstName: parsedBody.data.firstName
+        }
+    })
+    res.json({
+        message: "Created"
+    })
+})
+
+router.post("/sign-in", (req, res) => {
 
 })
 
-export {router as UserRouter};
+router.post("/get-user", (req, res) => {
+
+})
+
+export { router as UserRouter };
